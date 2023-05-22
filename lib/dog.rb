2cbbb32bcc1fb3b555f13f63cd5880aa84987c6e
Thomas <thomaskambale1@gmail.com>
2023-05-22 09:17:00 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
-class Dog
+require 'pry'
 
+class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+# dog_name = Dog.new
+# dog_name.name = "BonsaiCF"
+# dog_breed = Dog.new
+# dog_breed.name = "Mastiff"
+
+#binding.pry
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
-class Person
+require 'pry'
 
+class Person
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 end
+
+# person_name = Person.new
+# person_name.name = "BonsaiCF"
+# person_job = Person.new
+# person_job.name = "Full Stack Developer"
+
+#binding.pry
